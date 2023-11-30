a74f2fbbfcbb896b83f60895014474945c2af30b
Casey <leptone55@gmail.com>
2023-11-30 12:58:17 -0800

    shading

--- src/app/components/ThreeScene.tsx
+++ src/app/components/ThreeScene.tsx
@@ -13,7 +13,7 @@ import {
   useGLTF,
   useHelper,
 } from '@react-three/drei'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 // import hub from '@ezkljs/hub'
 // import { parse } from 'path'
 // import ThreeScene from './components/ThreeScene'
@@ -35,31 +35,19 @@ export default function ThreeScene({
   const sunDistance = 5 // Adjust as needed for the scale of your scene
 
   // Calculate sun's position
-  const sunPosition = {
-    x: sunDistance * Math.cos(tiltAngle),
-    y: sunDistance * Math.sin(tiltAngle),
-    z: 0, // Assuming Sun is positioned directly over the equator at equinox
-  }
-
-  // useHelper(directionalLightRef, DirectionalLightHelper, 1, 'red')
-
-  // useFrame(async () => {
-  //   const artifactId = '8c8791df-3f10-4d16-9774-f7e008a5cc7c'
-  //   const input = { input_data: [[1.0, 0.0, 0.0, 1.0]] }
-  //   const inputFile = JSON.stringify(input)
-
-  //   console.log('inputFile', inputFile)
-  //   const url = 'https://hub-staging.ezkl.xyz/graphql'
-
-  //   const resp = await hub.initiateProof({
-  //     artifactId,
-  //     inputFile,
-  //     url,
-  //   })
-  //   console.log('resp', resp)
-  //   // if (modelRef.current) {
-  //   //   modelRef.current.rotation.y += 0.01 // Adjust the rotation speed as needed
-  //   // }
+  const sunPosition = useMemo(
+    () => ({
+      x: sunDistance * Math.cos(tiltAngle),
+      y: sunDistance * Math.sin(tiltAngle),
+      z: 0, // Assuming Sun is positioned directly over the equator at equinox
+    }),
+    [tiltAngle]
+  )
+  // const sunPosition = {
+  //   x: sunDistance * Math.cos(tiltAngle),
+  //   y: sunDistance * Math.sin(tiltAngle),
+  //   z: 0, // Assuming Sun is positioned directly over the equator at equinox
+  // }
 
   const ticks = useRef(0)
 
@@ -84,11 +72,11 @@ export default function ThreeScene({
 
   return (
     <>
-      <ambientLight intensity={0.4} />
+      <ambientLight intensity={1.2} />
       <directionalLight
         ref={directionalLightRef}
         position={[sunPosition.x, sunPosition.y, sunPosition.z]}
-        intensity={7}
+        intensity={1.5}
       />
       <primitive ref={modelRef} object={model.scene} position={[0, -1, 0]} />
       <Stars
